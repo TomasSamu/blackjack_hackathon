b61f48004f0bbee1d7c316e6d1642df518acbd1f
Frantisek91 <Frantisek.Demi@gmail.com>
2019-02-01 10:12:21 +0100

    new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,11 @@ function htmlPublish(done) {
       .pipe(livereload());
 }
 
+function jsPublish(done) {
+    return gulp.src('src/assets/**/*.js')
+      .pipe(gulp.dest('public'));
+}
+
 // compile SCSS files
 function scssCompile(done) {
     return gulp.src('src/scss/**/*.scss')
@@ -49,6 +54,7 @@ function scssCompile(done) {
 // watch files
 function watchFiles(done) {
     gulp.watch("src/assets/**/*.html", htmlPublish);
+    gulp.watch("src/assets/**/*.js", jsPublish);
     gulp.watch("src/scss/**/*.scss", scssCompile);
 }
 
